26eb78cc84dd1fab3f8947fa92fb8439fa3269cd
mastroni-edu <mastroni@usc.edu>
2022-10-28 13:29:19 -0700

    Initial commit

---
+++ Assets/Scripts/EnvironmentStates.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentStates : MonoBehaviour
+{
+
+    public enum SampleStates { StateOne, StateTwo, StateThree };
+    public SampleStates mySampleState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Check for specific keyboar/mouse input
+            // Change mySampleState based on input 
+        
+    }
+}
---
+++ Assets/Scripts/ObjectStates.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectStates : MonoBehaviour
+{
+
+    public enum SampleStates { StateOne, StateTwo };
+    public SampleStates mySampleState;
+
+    float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Tick up the timer with Time.deltaTime
+            // Change mySampleState based on input 
+        
+    }
+}
